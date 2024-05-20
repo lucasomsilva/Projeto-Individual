e744c156f49c487a78d197dce0575d8ec573f2ce
lucasomsilva <lucas.msilva@sptech.school>
2024-05-20 14:13:39 -0300

    Arrumando gráfico de horas jogadas

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -7,14 +7,14 @@ const grafico2 = document.getElementById('grafico2');
 const grafico3 = document.getElementById('grafico3');
 
 new Chart(grafico1, {
-    type: 'line',
+    type: 'bar',
     data: {
-        labels: ['2018', '2019', '2020', '2021', '2022', '2023', '2024'],
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
         datasets: [{
             label: 'Valores em Milhões',
             backgroundColor: 'rgb(255, 20, 147)',
             borderColor: 'rgb(255, 20, 147)',
-            data: [30, 29, 28, 25, 22, 23, 30],
+            data: [30, 29, 28, 25, 22, 23, 30, 24, 40, 43, 34, 35],
         }],
     },
 });
@@ -31,7 +31,21 @@ new Chart(grafico2, {
                 '#4F0F0F'],
             data: [40, 30, 15, 15],
         }],
-    },
+    }, options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Horas jogadas por mês',
+                font: {
+                    size: 12
+                },
+                padding: {
+                    top: 3,
+                    bottom: 3
+                }
+            }
+        },
+    }
 });
 
 new Chart(grafico3, {
@@ -45,7 +59,21 @@ new Chart(grafico3, {
                 '#4F0F0F'],
             data: [40, 30, 15, 15],
         }],
-    },
+    }, options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Personagem preferido dos usuários',
+                font: {
+                    size: 12
+                },
+                padding: {
+                    top: 3,
+                    bottom: 3
+                }
+            }
+        },
+    }
 });
 
 listaPerguntas = [
@@ -57,7 +85,7 @@ function abrirModalPersonagem() {
     modal.classList.add('abrir');
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
             modal.classList.remove('abrir')
         }
     });
@@ -69,7 +97,7 @@ function abrirModalQuiz() {
     modal.classList.add('abrir');
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
             modal.classList.remove('abrir')
         }
     });
