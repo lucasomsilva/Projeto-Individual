6d51ac2f0536957716f9045d705e8e66bfb4cc3c
lucasomsilva <lucas.msilva@sptech.school>
2024-05-27 23:10:27 -0300

    Arrumando select de personagens

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -1,42 +1,58 @@
 const listaDescricaoPersonagens = [
-    'Assim como Arthur Morgan, você é um indivíduo multifacetado com uma vida interior rica. Sua força, compaixão e complexidade fazem de você alguém que deixa um impacto duradouro nas pessoas ao seu redor. Continue sendo você mesmo!', 
-    'Assim como John, você valoriza profundamente suas relações pessoais e faz o possível para proteger e cuidar daqueles que ama. Seu espírito aventureiro e sua capacidade de enfrentar o desconhecido com bravura são características que fazem de você uma pessoa única e admirável.', 
-    'Dutch van Der Linde', 
-    'Micah Bell', 
-    'Hosea Mattews',
-    'Lenny Summers', 
-    'Sadie Adler', 
-    'Javier Escuella', 
-    'Bil Williamson', 
-    'Uncle', 
-    'Charles Smith'
+    'Você é uma pessoa complexa e profundamente reflexiva, que valoriza a lealdade e a moralidade, mesmo em meio a circunstâncias difíceis. Você acredita na possibilidade de mudança e é uma pessoa que se esforça para encontrar o equilíbrio entre o bem e o mal, a lealdade e a justiça.',
+
+    'Você valoriza profundamente suas relações pessoais e faz o possível para proteger e cuidar daqueles que ama. Seu espírito aventureiro e sua capacidade de enfrentar o desconhecido com bravura são características que fazem de você uma pessoa única e admirável.', 
+
+    'Você é uma pessoa carismática e visionária, com um talento natural para liderar e inspirar os outros. Possui uma mente estratégica e está sempre em busca de um propósito maior além de seu entusiasmo e confiança serem contagiantes, porém nção enxergando as consequências de suas ações.', 
+
+    'Você é uma pessoa astuta e engenhosa, capaz de se adaptar rapidamente a diferentes situações. Sabe como manipular as circunstâncias além de ser muito determinado. Você pode ser visto como alguém que valoriza mais seus próprios interesses do que os dos outros', 
+
+    'Você é uma pessoa sagaz e experiente conhecido por sua paciência e diplomacia, com uma mente afiada e muita sabedoria. Um estrategista nato, planeja cuidadosamente seus movimentos. Sua capacidade de analisar situações complexas o tornam um conselheiro valioso e confiável.',
+
+    'Você é uma pessoa inteligente e corajosa, com um espírito jovem e uma mente curiosa, sempre está buscando aprender e crescer. Sua energia e entusiasmo são contagiosos e sua capacidade de se conectar com os outros e ter uma amizade genuína faz de você um amigo valioso e leal.',
+
+    'Você é capaz de enfrentar desafios difíceis com bravura e resiliência, visto como alguém que não recua diante da adversidade, além de ser alguém que valoriza sua liberdade e autonomia. Sua personalidade é marcada por uma combinação de força e compaixão',
+
+    'Você é uma pessoa calma e centrada, com um grande senso de propósito e determinação. Você é capaz de manter a postura mesmo nas situações mais difíceis. Sua capacidade de pensar com clareza sob pressão e de tomar decisões certas faz de você alguém confiável e respeitado.',
+
+    'Você é conhecido por sua natureza impulsiva e sua disposição para correr riscos. Você compartilha essas características, sendo alguém que não tem medo de tomar decisões difíceis, mesmo quando as probabilidades estão contra você, dendo determinado e tendo uma boa capacidade de liderança',
+
+    'Você é uma pessoa descontraída e de espírito livre, que valoriza a liberdade e a simplicidade da vida. Prefere evitar conflitos e aproveitar o momento. Sua natureza pacífica e sua capacidade de encontrar alegria nas pequenas coisas fazem de você alguém que é fácil de se conviver.',
+
+    'Você é uma pessoa gentil e compassiva, com um grande coração e uma disposição para ajudar os outros. Você é alguém que valoriza a empatia e a bondade. Conhecido por sua calma e sabedoria, e com uma combinação entre compaixão e coragem de ajudar o próximo.',
+
+    'Você é uma pessoa extrovertida e cheia de vida, com um espírito aventureiro e uma personalidade cativante. Você é alguém que valoriza a liberdade e a diversão. Sua energia e entusiasmo são contagiantes, além de ser muito espontâneo e animado, se jogando de cabeça em novos desafios.'
 ];
 
 function selecionarPersonagem() {
-    let personagemSelecionado = select_personagens.value;
+    let selectPersonagens = document.getElementById('select_personagens');
+    let personagemSelecionado = selectPersonagens.value;
+    let paragrafoPersonagem = document.getElementById('paragrafo_personagem');
 
-    if (personagemSelecionado = "arthur") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[0]}`;
-    } else if (personagemSelecionado == "jhon") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[1]}`;
-    } else if (personagemSelecionado = "dutch") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[2]}`;
-    } else if (personagemSelecionado = "micah") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[3]}`;
-    } else if (personagemSelecionado = "hosea") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[4]}`;
-    } else if (personagemSelecionado = "lenny") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[5]}`;
-    } else if (personagemSelecionado = "sadie") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[6]}`;
-    } else if (personagemSelecionado = "javier") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[7]}`;
-    } else if (personagemSelecionado = "bill") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[8]}`;
-    } else if (personagemSelecionado = "uncle") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[9]}`;
-    } else if (personagemSelecionado = "charles") {
-        paragrafo_personagem.innerHTML = `${listaDescricaoPersonagens[10]}`;
+    if (personagemSelecionado === "arthur") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[0]}`;
+    } else if (personagemSelecionado === "jhon") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[1]}`;
+    } else if (personagemSelecionado === "dutch") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[2]}`;
+    } else if (personagemSelecionado === "micah") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[3]}`;
+    } else if (personagemSelecionado === "hosea") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[4]}`;
+    } else if (personagemSelecionado === "lenny") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[5]}`;
+    } else if (personagemSelecionado === "sadie") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[6]}`;
+    } else if (personagemSelecionado === "javier") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[7]}`;
+    } else if (personagemSelecionado === "bill") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[8]}`;
+    } else if (personagemSelecionado === "uncle") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[9]}`;
+    } else if (personagemSelecionado === "charles") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[10]}`;
+    } else if (personagemSelecionado === "sean") {
+        paragrafoPersonagem.innerHTML = `${listaDescricaoPersonagens[11]}`;
     }
 }
 
